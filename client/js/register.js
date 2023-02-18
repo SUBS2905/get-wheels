a0758fc5106f22340ac279feace5e5d48e27c339
Subhransu <subhranshumajhi2@gmail.com>
2023-02-18 12:02:22 +0530

    email field update

--- client/js/register.js
+++ client/js/register.js
@@ -1,12 +1,14 @@
 const sendBtn = document.querySelector(".send");
 const verifyBtn = document.querySelector(".verify");
 const inputOTP = document.querySelector("#input-otp");
+const otpSent = document.querySelector(".otp-sent");
 
 const handleSend = (e) => {
   e.preventDefault();
   verifyBtn.style.display = "block";
   inputOTP.style.display = "block";
   sendBtn.style.display = "none";
+  otpSent.style.display = "block";
 };
 
 sendBtn.addEventListener("click", handleSend);
