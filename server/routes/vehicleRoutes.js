9e4d0b3d9b4284358edec7ab0bfe9c6c29080433
Subhransu <subhranshumajhi2@gmail.com>
2023-05-14 14:01:16 +0530

    update method to POST

--- server/routes/vehicleRoutes.js
+++ server/routes/vehicleRoutes.js
@@ -5,7 +5,7 @@ const vehController = require("../controllers/vehController");
 
 router.get("/getallvehicles", vehController.getAllVehicles);
 // router.get("/getallvehiclesbyowner/:ownerId", vehController.getAllVehiclesByOwnerId);
-router.get("/getvehiclesbyid", vehController.getVehicleByVehicleId);
+router.post("/getvehiclesbyid", vehController.getVehicleByVehicleId);
 router.post("/addvehicle", vehController.addVeh);
 // router.put("/editvehicle", vehController.editVeh);
 // router.post("/deletevehicle", vehController.deleteVeh);
